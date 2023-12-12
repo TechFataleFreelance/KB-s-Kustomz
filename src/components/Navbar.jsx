fc615c810624c8b45f144abeb039eb773a23c812
Kylee <Kykybrown2002@gmail.com>
2023-12-12 10:39:28 -0600

    changed logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
         <>
             <motion.nav animate={{ opacity:[0,1]}} viewport={{once:true}} transition={{duration:1, delay:1}} class="sticky z-10 bg-gray-200 top-0 w-full h-[15%]">
             <Link path to='/'>
-                <motion.img whileHover={{scale:1.05}} transition={{type:'spring', duration: .7}} alt="KB's Kustomz custom paint and autobody logo" className=" sm:w-[95px] text-white text-[8px] absolute top-[-4.8px] pb-[0px] xs:w-[65px] md:w-[110px] xl:w-[130px] lg:w-[130px] " src="/Images/Logo(1).png"></motion.img>
+                <motion.img whileHover={{scale:1.05}} transition={{type:'spring', duration: .7}} alt="KB's Kustomz custom paint and autobody logo" className=" sm:w-[95px] text-white text-[8px] absolute top-[-4.8px] pb-[0px] xs:w-[65px] md:w-[110px] xl:w-[130px] lg:w-[130px] " src="public/Images/logo.png"></motion.img>
             </Link>
             
                 <div class="flex items-center justify-end text-white font-glacialBold bg-black w-full h-[30%]">
